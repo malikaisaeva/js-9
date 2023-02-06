7f008b9fb7e3a43cf39390c89e53ef2ccae26a62
malikaisaeva <121940266+malikaisaeva@users.noreply.github.com>
2023-02-06 19:27:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+let user = {
+    name: 'Malika',
+    surename: 'Isaeva',
+    age: 15,
+    school: 35,
+    class: 9,
+    address: 'sattepo',
+    phone: 998915561095,
+    travel: {
+        visa: ['Rossia', 'Turkey']
+    }
+}
+
+let car = {
+    name: 'captiva',
+    number: 'U947NA',
+    color: 'milk',
+    year: 2017
+}
+
+let newObject = Object.assign({}, user, {car})
+let keys = Object.keys(newObject)
+let bio = Object.values(newObject)
+
+let arr = keys.concat(bio)
+
+console.log(newObject);
+console.log(arr);
+
+let types = {
+    String: [arr.filter(String)],
+    number: [arr.filter(Number)],
+    object: [arr.filter(Object)]
+}
+
